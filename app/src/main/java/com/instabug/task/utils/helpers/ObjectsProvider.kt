fec485d3ff8b8f34e7a5db0f91f83aa0c43767a0
ahmed_ezz012 <ahmed_ezz2021995@yahoo.com>
2020-12-17 22:50:59 +0200

    add utils and objects provider

---
+++ app/src/main/java/com/instabug/task/utils/helpers/ObjectsProvider.kt
@@ -0,0 +1,25 @@
+package com.instabug.task.utils.helpers
+
+import android.content.Context
+import com.instabug.task.data.locale.LocalDataSource
+import com.instabug.task.data.remote.RemoteDataSource
+import com.instabug.task.ui.words.WordsRepository
+import com.instabug.task.utils.Constants
+
+object ObjectsProvider {
+    fun provideSQLiteHelper(context: Context): SQLiteHelper {
+        return SQLiteHelper(context, Constants.DataBase.DB_NAME, Constants.DataBase.DB_VERSION)
+    }
+
+    fun provideLocalDataSource(context: Context): LocalDataSource {
+        return LocalDataSource(provideSQLiteHelper(context))
+    }
+
+    fun provideRemoteDataSource(): RemoteDataSource {
+        return RemoteDataSource()
+    }
+
+    fun provideWordsRepository(context: Context): WordsRepository {
+        return WordsRepository(context, provideRemoteDataSource(), provideLocalDataSource(context))
+    }
+}
